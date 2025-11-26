bf5f0155ebc198fec73f12131af412f11780ec77
Andrii Hlyvko <andriihlyvko@gmail.com>
2025-11-25 22:36:40 -0800

    header struct

--- src/protocol/parser.rs
+++ src/protocol/parser.rs
@@ -3,7 +3,18 @@
 
 
 pub struct Header {
-    field_1: i32,
+    pub identifier: u16,//A random ID assigned to query packets. Response packets must reply with the same ID.
+    pub query_or_response: bool,// 1 for reply packet, 0 for a question packet
+    pub opcode: u8, // 4 bits
+    pub authoritative: bool,
+    pub truncation: bool,
+    pub recursion_desired: bool,
+    pub recursion_available: bool,
+    pub response_code: u8, // 4bit response code + 3bit reserved
+    pub question_count: u16,
+    pub answer_record_count: u16,
+    pub authority_record_count: u16,
+    pub additional_record_count: u16,
 }
 
 pub struct DNSPacket {
