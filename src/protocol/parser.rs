9d9d27d895334b8744cb7ecac5d9d3cdd538624c
Andrii Hlyvko <andriihlyvko@gmail.com>
2025-11-25 22:43:56 -0800

    additional method

--- src/protocol/parser.rs
+++ src/protocol/parser.rs
@@ -1,7 +1,7 @@
 
 
 
-
+/// Header is always 12bytes inside a raw packet
 pub struct Header {
     pub identifier: u16,//A random ID assigned to query packets. Response packets must reply with the same ID.
     pub query_or_response: bool,// 1 for reply packet, 0 for a question packet
@@ -10,11 +10,20 @@ pub struct Header {
     pub truncation: bool,
     pub recursion_desired: bool,
     pub recursion_available: bool,
-    pub response_code: u8, // 4bit response code + 3bit reserved
+    pub rezerved: u8,
+    pub response_code: u8, // 4bit response code
     pub question_count: u16,
     pub answer_record_count: u16,
     pub authority_record_count: u16,
     pub additional_record_count: u16,
+}
+
+impl Header {
+    
+    pub fn from_bytes(data: &[u8])->Option<Self> {
+        None
+    }
+    
 }
 
 pub struct DNSPacket {
